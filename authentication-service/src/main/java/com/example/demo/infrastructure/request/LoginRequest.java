e2feb2dc7d45089911e4453130183d6a9dab1670
L0KODOM <diedom4business@gmail.com>
2025-02-25 11:11:10 +0100

    Primer commit - Subiendo microservicios

--- authentication-service/src/main/java/com/example/demo/infrastructure/request/LoginRequest.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.infrastructure.request;
-
-public record LoginRequest(
-	String email,
-	String password
-	) {
-
-}
--- authentication-service/src/main/java/com/example/demo/infrastructure/request/RegisterRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.infrastructure.request;
-
-public record RegisterRequest(
-		String email,
-		String password,
-		String name
-	) {
-
-}
